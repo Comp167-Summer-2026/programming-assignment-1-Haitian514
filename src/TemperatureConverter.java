09d4ecada84fcae4b062c24e07e60407a01a2f4c
Haitian514 <seanrtelemaque@gmail.com>
2026-06-07 22:09:21 -0400

    Adding code from InteliJ to fix commit issue

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,16 +1,56 @@
+import java.util.Scanner;
+
 public class TemperatureConverter {
 
-    /* hint
-
     public static double convertTemperature(double temperature, String unit) {
-        // TODO: students implement this
-        return 0.0;
+        if (unit.equalsIgnoreCase("C")) {
+            return (temperature * 9.0 / 5.0) + 32;
+        }
+        else {
+            return (temperature - 32) * 5.0 / 9.0;
+        }
     }
-
-     */
 
     public static void main(String[] args) {
-        // TODO: students implement this
-    }
+        Scanner scnr = new Scanner(System.in);
 
+        String input = "";
+
+        while (!input.equalsIgnoreCase("stop")) {
+
+            System.out.print("Enter a temperature value or type stop to quit: ");
+            input = scnr.nextLine();
+
+            if (input.equalsIgnoreCase("stop")) {
+                System.out.println("Program ended.");
+            }
+            else if (!input.matches("-?\\d+(\\.\\d+)?")) {
+                System.out.println("Error: Invalid temperature input.");
+            }
+            else {
+                double temperature = Double.parseDouble(input);
+
+                System.out.print("Enter unit (C or F): ");
+                String unit = scnr.nextLine();
+
+                if (unit.equalsIgnoreCase("C")) {
+                    double converted = convertTemperature(temperature, unit);
+
+                    System.out.printf("%.2f°C is equal to %.2f°F%n",
+                            temperature, converted);
+                }
+                else if (unit.equalsIgnoreCase("F")) {
+                    double converted = convertTemperature(temperature, unit);
+
+                    System.out.printf("%.2f°F is equal to %.2f°C%n",
+                            temperature, converted);
+                }
+                else {
+                    System.out.println("Error: Invalid unit. Enter C or F.");
+                }
+            }
+        }
+
+        scnr.close();
+    }
 }
